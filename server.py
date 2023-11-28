ccaef1754cc5711df4e4d305bd7df28f2b962c67
GIOPIYO <112946220+GIOPIYO@users.noreply.github.com>
2023-11-28 14:12:01 +0300

    simple client server model

---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+
+def handle_connection(client_socket):
+    
+    allowed_ips = ['127.0.0.1'] 
+
+    client_address = client_socket.getpeername()[0]
+    if client_address not in allowed_ips:
+        print(f"Connection from {client_address} rejected (firewall rule)")
+        return
+
+    print('Got connection from', client_address)
+
+    message = 'Thank you for connecting'
+    client_socket.send(message.encode())
+    client_socket.close()
+
+def main():
+    s = socket.socket()
+    port = 3456
+
+    try:
+        s.bind(('', port))
+        print(f'Socket binded to port {port}')
+    except socket.error as e:
+        print(str(e))
+        return
+
+    s.listen(5)
+    print('Socket is listening')
+
+    while True:
+        try:
+            c, addr = s.accept()
+            handle_connection(c)
+        except socket.error as e:
+            print(str(e))
+            break
+
+    s.close()
+
+if __name__ == "__main__":
+    main()
